cc934764f4a0986a1d772e28180d8731a9ad1246
Anton_Demidov <aademidov@list.ru>
2020-04-28 03:53:17 +0300

    Добавьте файлы проекта.

---
+++ ServerAndroid/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ServerAndroid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lstStatus = new System.Windows.Forms.ListBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(151, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(45, 11);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.Text = "192.168.0.105";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(45, 37);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "5000";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(151, 35);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lstStatus
+            // 
+            this.lstStatus.FormattingEnabled = true;
+            this.lstStatus.Location = new System.Drawing.Point(272, 63);
+            this.lstStatus.Name = "lstStatus";
+            this.lstStatus.Size = new System.Drawing.Size(287, 147);
+            this.lstStatus.TabIndex = 7;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(10, 217);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(188, 42);
+            this.btnScan.TabIndex = 8;
+            this.btnScan.Text = "Определить местоположение";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(10, 63);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(256, 148);
+            this.txtStatus.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 275);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.lstStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ServerAndroid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lstStatus;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
---
+++ ServerAndroid/Form1.cs
@@ -0,0 +1,127 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerAndroid
+{
+    public partial class Form1 : Form
+    {
+        public struct message // объявляем структуру для массива
+        {
+            public string NAME;
+            public string UUID;
+            public string RSSI;
+        } 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer(); 
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+        }
+
+        public void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            lstStatus.Invoke((MethodInvoker)delegate () //получение сообщения
+            {
+                //lstStatus.Items.Add(e.MessageString); // заполняем lstStatus
+                txtStatus.Text += e.MessageString; // заполняем lstStatus
+                e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+            });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //старт сервера
+            //txtStatus.Text += "Server running";
+            btnStart.BackColor = Color.GreenYellow;//кнопка стоп - зеленая
+            btnStop.BackColor = Color.IndianRed;//кнопка стоп - зеленая
+            btnStop.Enabled = true;//кнопка стоп - неактивна
+            btnStart.Enabled = false;//кнопка старт - активна
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text);
+            server.Start(ip, Convert.ToInt32(txtPort.Text));// запускаем сервер по заданному айпи и порту
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)// если запущен останавливаем
+            {
+                server.Stop();// выключаем сервер
+                btnStop.BackColor = Color.GreenYellow; //кнопка стоп - зеленая
+                btnStart.BackColor = Color.IndianRed;//кнопка старт - красная
+                btnStop.Enabled = false; //кнопка стоп - неактивна
+                btnStart.Enabled = true;//кнопка старт - активна
+                // txtStatus.Text += "Server stopped";
+            }
+
+        }
+
+        public void btnScan_Click(object sender, EventArgs e)
+        {
+            message[] infoMess = new message[1000]; // объявление массив структуры для хранения данных с телефона и выделить память для 1000 структур типа message
+            string tempstr = ""; // временная переменная дл сохранения инфорация ДО нахождения пробела
+            int tempfr = -1;// переменнная для подсчета фрагментов, всего их 3 как и пробелов в одном сообщение
+            string tempNAME = ""; // временная переменная для хранения имени владельца МУ (мобильного устройства)
+            string tempUUID = "";// временная переменная для хранения айди метки
+            string str = txtStatus.Text;// переменная для поиска пробелов
+            int temp=-1; //временная переменная для заполнения массива
+
+
+            for (int i = 0; i < str.Length; i++) // перебираем строку получаемого сообщения от телефона
+            {
+                if (str[i] == ' ') //поиск пробела
+                {
+                    tempfr += 1;// прошли один фрагмент
+
+                    if (tempfr == 0) // 0 фрагмент имя устройства
+                        tempNAME = tempstr; //заполняем временную переменную name 
+
+                    if (tempfr == 1) // 1 фрагмент uuid устройства
+                        tempUUID = tempstr;
+
+                    if (tempfr == 2)// 2 фрагмент уровень сигнала rssi устройства до маяка
+                    {
+                        temp += 1;//следующая ячейка массива
+                        infoMess[temp].NAME = tempNAME; //заполняем структуру массива
+                        infoMess[temp].UUID = tempUUID;
+                        infoMess[temp].RSSI = tempstr;
+                        tempfr = -1;//скидываем счетчик фрагмента
+                    }
+
+                    tempstr = ""; //обнуляем временную строку
+                }
+                else// пока не нашли пробела
+                {
+                    tempstr += str[i];// заполянем временную строку
+                }
+            }
+
+           lstStatus.Items.Clear();// очищаем listbox
+
+            for (int j = 0; j < infoMess.Length; j++) //перебираем массив
+            {
+                lstStatus.Items.Add(j + " | " + infoMess[j].NAME + " | " + infoMess[j].UUID + " | " + infoMess[j].RSSI);//заполянем listbox данными из массива
+            }
+
+
+
+
+        }
+    }
+}
+
+
+         
